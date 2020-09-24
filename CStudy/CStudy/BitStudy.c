0a1e6d090de89d6b403db4ebe0b88a1e53357784
plone931206 <plone931206@gmail.com>
2020-09-24 14:21:34 +0900

    비트연산자

---
+++ CStudy/CStudy/BitStudy.c
@@ -0,0 +1,45 @@
+﻿#include<stdio.h>
+
+int main() {
+	int num1 = 1;// 0000 0001
+	int num2 = 5;// 0000 0101
+
+	printf("%d\n", num1 & num2);//AND : 0000 0001
+	printf("%d\n", num1 | num2);//OR : 0000 0101
+	printf("%d\n", num1 ^ num2);//XOR : 0000 0100
+
+	return 0;
+}
+
+/*
+임베디드에서의 사용방식
+
+0 : 전류 차단
+1 : 전류 통과
+
+포트번호 : 8765 4321
+동작신호 : 0000 0000
+
+
+포트번호 1 : 첫번째 LED ON
+포트번호 2 : 두번째 LED ON
+..
+..
+
+이라고 가정하면
+
+//초기값
+int keyPin = 0; // 0000 0000
+
+//LED컨트롤
+int LED1 = 1; //0000 0001, 첫번째 LED를 키는 신호
+int LED2 = 2; //0000 0010, 두번째 LED를 키는 신호
+int OFF = 0; //0000 0000, 모든 LED를 끔
+
+keyPin = keyPin | LED1; //1번 LED ON
+keyPin = keyPin | LED2; //2번 LED ON
+
+keyPin = OFF; //LED 전부 끄기
+
+
+*/
---
+++ CStudy/CStudy/Data.c
@@ -0,0 +1,39 @@
+﻿/*
+char : -128 ~ 127
+short int : -32768 ~ 32767
+int 
+long int : -2147483648 ~ 2147483647
+
+
+정수
+unsigned char : 0 ~ 256 
+unsigned short int : 0 ~ 65535
+unsigned int
+unsigned long int : 0 ~ 4294967295
+
+실수
+float
+double
+long double
+
+*/
+
+#include<stdio.h>
+
+int sum(int, int);//프로토 타입
+
+int main() {//메인
+	int A = 1;
+	int B = 2;
+	int result = sum(A, B);
+
+	printf("A+B = %d", result);
+
+	return 0;
+}
+
+int sum(int A, int B) {//함수
+	int result = A + B;
+
+	return result;
+}
