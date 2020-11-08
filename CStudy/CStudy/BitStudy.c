03ea193669481fe3e24346f2c51b0463ead855c4
plone931206 <plone931206@gmail.com>
2020-11-08 12:51:13 +0900

    2020-11-08

--- CStudy/CStudy/BitStudy.c
+++ CStudy/CStudy/BitStudy.c
@@ -1,12 +1,17 @@
 ﻿#include<stdio.h>
 
 int main() {
-	int num1 = 1;// 0000 0001
-	int num2 = 5;// 0000 0101
+    int* numPtr1;     // 단일 포인터 선언
+    int* numPtr2;    // 이중 포인터 선언
+    int num1 = 10;
 
-	printf("%d\n", num1 & num2);//AND : 0000 0001
-	printf("%d\n", num1 | num2);//OR : 0000 0101
-	printf("%d\n", num1 ^ num2);//XOR : 0000 0100
+    numPtr1 = &num1;    // num1의 메모리 주소 저장 
+
+    numPtr2 = &numPtr1; // numPtr1의 메모리 주소 저장
+
+    printf("%d\n", *numPtr2);    // 20: 포인터를 두 번 역참조하여 num1의 메모리 주소에 접근
+
+
 
 	return 0;
 }
---
+++ CStudy/PointerStudy/상수와 포인터.c
@@ -0,0 +1,15 @@
+﻿int main() {
+	const int num1 = 10;    // int형 상수
+	const int* numPtr;      // int형 상수를 가리키는 포인터. int const *numPtr도 같음
+
+	numPtr = &num1;
+	*numPtr = 20;    // 컴파일 에러. num1이 상수이므로 역참조로 값을 변경할 수 없음
+
+	int num1 = 10;    // int형 변수
+	int num2 = 20;    // int형 변수
+	int* const numPtr = &num1;    // int형 포인터 상수
+
+	numPtr = &num2;    // 컴파일 에러. 포인터(메모리 주소)를 변경할 수 없음
+
+	return 0;
+}
